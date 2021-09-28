7d4168f3232f09792db9995d400ff7b51fb15e35
JhyunJung <jhyun.rigel@gmail.com>
2021-09-28 23:01:50 +0900

    Regex update

---
+++ week01/정규표현식/jhyun.py
@@ -0,0 +1,12 @@
+# Matching Anything But a Newline
+
+regex_pattern = r"^.{3}\..{3}\..{3}\..{3}$"
+
+# Matching Digits & Non-Digit Characters
+
+Regex_Pattern = r"^.{2}\D.{2}\D.{4}"
+
+# Matching Whitespace & Non-Whitespace Character
+
+Regex_Pattern = r"\S{2}\s\S{2}\s\S{2}"	# Do not delete 'r'.
+
