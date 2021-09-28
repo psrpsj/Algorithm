cf1bc9fee89585012e745c3d009d94959d3e5b92
Yunsung Choi <jws02025@gmail.com>
2021-09-29 00:01:09 +0900

    Update Regex

---
+++ week01/정규표현식/yunsung.py
@@ -0,0 +1,14 @@
+# Matching Anything But a Newline
+regex_pattern = r"^.{3}\..{3}\..{3}\..{3}$"
+
+# Matching Digits & Non-Digit Characters
+Regex_Pattern = r"\d{2}\D\d{2}\D\d{4}"
+
+# Matching Whitespace & Non-Whitespace Character
+Regex_Pattern = r"\S{2}\s\S{2}\s\S{2}"
+
+# Matching Word & Non-Word Character
+Regex_Pattern = r"\w{3}\W\w{10}\W\w{3}"
+
+# Matching Start & End
+Regex_Pattern = r"^\d\w{4}\.$"
