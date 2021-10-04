6af6993fef62a59a68899e62624f25ab5e7b9d13
Yunsung Choi <jws02025@gmail.com>
2021-10-05 01:51:14 +0900

    Update Regex

---
+++ week02/정규표현식/yunsung.py
@@ -0,0 +1,11 @@
+# Matching {x, y} Repetitions
+Regex_Pattern = r'^\d{1,2}[a-zA-Z]{3,}\.{0,3}$'
+
+# Matching Zero Or More Repetitions
+Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'
+
+# Matching One Or More Repetitions
+Regex_Pattern = r'^\d+[A-Z]+[a-z]+$'
+
+# Matching Ending Items
+Regex_Pattern = r'^[a-zA-Z]*s$'
